9dacb7aad4536d5e0bebf79dc0dd6bf262242e05
onlineUserSD <tzsaad6@gmail.com>
2025-08-12 15:24:57 +0530

    Final Push for now

--- src/components/Skills/Skill.jsx
+++ src/components/Skills/Skill.jsx
@@ -39,16 +39,16 @@ const Skill = () => {
             transitionSpeed={1000}
             gyroscope={true}
           >
-            <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 w-full">
+            <div className="grid grid-cols-2 sm:grid-cols-2 gap-2 w-full">
               {category.skills.map((skill) => (
                 <div
                   key={skill.name}
-                  className="flex items-center justify-center space-x-2 bg-transparent border-2 border-gray-700 rounded-3xl py-2 px-2 sm:py-2 sm:px-2 text-center"
+                  className="flex items-center justify-center space-x-2 bg-transparent border-2 border-gray-700 rounded-3xl py-2 px-3 sm:py-2 sm:px-2 text-center"
                 >
                   <img
                     src={skill.logo}
                     alt={`${skill.name} logo`}
-                    className="w-6 h-6 sm:w-7 sm:h-8"
+                    className="w-4 h-6 sm:w-7 sm:h-8"
                   />
                   <span className="text-xs sm:text-sm text-gray-300">
                     {skill.name}
